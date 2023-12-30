e3afee6cfc295b473517815671ab806ab1552307
Kirill Taran <kirilltaran@hotmail.com>
2023-12-30 13:13:23 +0400

    Not Found page styled

---
+++ Warp.WebApp/Pages/Shared/_Social.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Warp.WebApp.Pages.Shared
+{
+    public class _SocialModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
