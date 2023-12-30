0d1af395ef1428fc391610b486f884737dbcb954
Kirill Taran <kirilltaran@hotmail.com>
2023-12-30 15:46:16 +0400

    Logo styles added

---
+++ Warp.WebApp/Pages/Shared/_Logo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Warp.WebApp.Pages.Shared
+{
+    public class _LogoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
