8c61efc93a0515af5b3f630c1ad3656341539263
Kirill Taran <kirilltaran@hotmail.com>
2023-12-30 15:35:04 +0400

    Primary button styles added

---
+++ Warp.WebApp/Pages/Shared/_BackButton.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Warp.WebApp.Pages.Shared
+{
+    public class _BackButtonModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
