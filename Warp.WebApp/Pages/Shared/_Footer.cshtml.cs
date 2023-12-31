17aa6c7da0a86fb14e88b7b0f4bd78e3cd183cab
Kirill Taran <kirilltaran@hotmail.com>
2023-12-31 21:20:17 +0400

    Layout rearranged

---
+++ Warp.WebApp/Pages/Shared/_Footer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Warp.WebApp.Pages.Shared
+{
+    public class _FooterModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Warp.WebApp/Pages/Shared/_Header.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Warp.WebApp.Pages.Shared
+{
+    public class _HeaderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
