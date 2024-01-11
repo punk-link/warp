9b6bce43e982148cf71e1b17b541af90f29e26ca
Kirill Taran <kirilltaran@hotmail.com>
2024-01-12 01:51:44 +0400

    Entry Url copy function added

--- Warp.WebApp/wwwroot/js/site.js
+++ Warp.WebApp/wwwroot/js/site.js
@@ -1,4 +1,8 @@
-﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
-
-// Write your JavaScript code.
+﻿function copyUrl() {
+    try {
+        document.body.style.cursor = 'wait';
+        navigator.clipboard.writeText(window.location.href);
+    } finally {
+        document.body.style.cursor = 'auto';
+    }
+}
