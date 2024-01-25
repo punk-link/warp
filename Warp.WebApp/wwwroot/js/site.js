3daa2741511a5a913a9a87b0de5bc6298f519f4b
Kirill Taran <kirilltaran@hotmail.com>
2024-01-25 06:49:58 +0400

    Image paste option added

--- Warp.WebApp/wwwroot/js/site.js
+++ Warp.WebApp/wwwroot/js/site.js
@@ -1,4 +1,16 @@
-﻿function cancelModal() {
+﻿function appendImage(blob) {
+    let image = document.createElement('img');
+    image.src = URL.createObjectURL(blob);
+
+    let imageContainer = document.getElementById('image-container');
+    imageContainer.append(image);
+
+    let textArea = document.getElementById('warp-content-textarea');
+    textArea.style.display = 'none';
+}
+
+
+function cancelModal() {
     let modal = document.getElementsByClassName('modal-background')[0];
     modal.style.display = 'none';
 }
@@ -10,6 +22,24 @@ function copyUrl() {
         navigator.clipboard.writeText(window.location.href);
     } finally {
         document.body.style.cursor = 'auto';
+    }
+}
+
+
+async function pasteImages() {
+    try {
+        await navigator.permissions.query({ name: 'clipboard-read' });
+
+        let clipboardItems = await navigator.clipboard.read();
+        for (let item of clipboardItems) {
+            let imageTypes = item.types.filter(type => type.startsWith('image/'));
+            for (let type of imageTypes) {
+                let blob = await item.getType(type);
+                appendImage(blob);
+            }
+        }
+    } catch(err) {
+        console.error(err);
     }
 }
 
