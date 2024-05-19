6304ed5f8d83e6793eda9ee2d82667016f71170d
Kirill Taran <kirilltaran@hotmail.com>
2024-05-19 14:31:18 +0400

    Google analytics tag added

---
+++ Warp.WebApp/Models/Options/AnalyticsOptions.cs
@@ -0,0 +1,6 @@
+﻿namespace Warp.WebApp.Models.Options;
+
+public class AnalyticsOptions
+{
+    public string GoogleGTag { get; init; } = string.Empty;
+}
---
+++ Warp.WebApp/Pages/Shared/Components/Analytics.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Warp.WebApp.Models.Options;
+
+namespace Warp.WebApp.Pages.Shared.Components;
+
+public class AnalyticsModel : PageModel
+{
+    public AnalyticsModel(AnalyticsOptions options)
+    {
+        GTag = options.GoogleGTag;
+    }
+
+
+    public void OnGet()
+    {
+    }
+
+
+    public string GTag { get; init; }
+}
