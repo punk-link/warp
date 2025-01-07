b9a9f7193e72318efa873fcbc691801339f3e337
Дмитрий Дмитрий <alemont1998@gmail.com>
2025-01-08 01:19:02 +0300

    OpenGraphTests fixes

--- Warp.WebApp.Tests/OpenGraphServiceTests.cs
+++ Warp.WebApp.Tests/OpenGraphServiceTests.cs
@@ -5,6 +5,7 @@ using Warp.WebApp.Models.Entries;
 using Warp.WebApp.Models.Entries.Enums;
 using Warp.WebApp.Services;
 using Warp.WebApp.Services.Infrastructure;
+using Warp.WebApp.Services.OpenGraph;
 
 namespace Warp.WebApp.Tests;
 
@@ -20,76 +21,48 @@ public class OpenGraphServiceTests
     }
 
 
-    //[Fact]
-    //public void GetDefaultDescription_ReturnsDefaultDescription()
-    //{
-    //    var service = new OpenGraphService(_localizer);
+    [Fact]
+    public void GetDefaultDescription_ReturnsDefaultDescription()
+    {
+        var service = new OpenGraphService(_localizer);
 
-    //    var result = service.GetDefaultDescription();
+        var result = service.GetDefaultDescription();
 
-    //    Assert.NotEmpty(result.Title);
-    //    Assert.Equal(DefaultDescription, result.Description);
-    //    Assert.NotNull(result.ImageUrl);
-    //}
-
-
-    //[Fact]
-    //public void BuildDescription_WithDescription_ReturnsProcessedDescription()
-    //{
-    //    var service = new OpenGraphService(_localizer);
-    //    var entry = new Entry(Guid.NewGuid(), Description, DateTime.Now, DateTime.Now, EditMode.Unknown, [], EntryOpenGraphDescription.Empty);
-
-    //    var result = service.BuildDescription(entry);
-
-    //    Assert.NotEmpty(result.Title);
-    //    Assert.Equal(Description, result.Description);
-    //    Assert.NotNull(result.ImageUrl);
-    //}
+        Assert.NotEmpty(result.Title);
+        Assert.Equal(DefaultDescription, result.Description);
+        Assert.NotNull(result.ImageUrl);
+    }
 
 
-    //[Theory]
-    //[InlineData("This is a very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis. very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis.", "This is a very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis. very long description that exceeds the maximum length allowed.")]
-    //[InlineData("This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be trimmed.", "This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be...")]
-    //[InlineData("This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis.", "This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be...")]
-    //public void BuildDescription_WithLongDescription_ReturnsProcessedDescription(string description, string expected)
-    //{
-    //    var service = new OpenGraphService(_localizer);
-    //    var entry = new Entry(Guid.NewGuid(), description, DateTime.Now, DateTime.Now, EditMode.Unknown, [], EntryOpenGraphDescription.Empty);
+    [Fact]
+    public void BuildDescription_WithDescription_ReturnsProcessedDescription()
+    {
+        var service = new OpenGraphService(_localizer);
 
-    //    var result = service.BuildDescription(entry);
+        var result = service.BuildDescription(Description, DefaultUri);
 
-    //    Assert.Equal(expected, result.Description);
-    //}
+        Assert.NotEmpty(result.Title);
+        Assert.Equal(Description, result.Description);
+        Assert.NotNull(result.ImageUrl);
+    }
 
 
-    //[Fact]
-    //public void BuildDescription_WithUrls_ReturnsModelWithProcessedUrl()
-    //{
-    //    var urls = new List<Uri> { new("https://example.com/image.jpg") };
-    //    var service = new OpenGraphService(_localizer);
-    //    var entry = new Entry(Guid.NewGuid(), Description, DateTime.Now, DateTime.Now, EditMode.Unknown, urls, EntryOpenGraphDescription.Empty);
+    [Theory]
+    [InlineData("This is a very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis. very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis.", "This is a very long description that exceeds the maximum length allowed. It should be trimmed and end with ellipsis. very long description that exceeds the maximum length allowed.")]
+    [InlineData("This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be trimmed.", "This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be...")]
+    [InlineData("This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis.", "This is a very long description that exceeds the maximum length allowed, it should be trimmed and end with ellipsis, very long description that exceeds the maximum length allowed, it should be...")]
+    public void BuildDescription_WithLongDescription_ReturnsProcessedDescription(string description, string expected)
+    {
+        var service = new OpenGraphService(_localizer);
+        var result = service.BuildDescription(description, DefaultUri);
 
-    //    var result = service.BuildDescription(entry);
-
-    //    Assert.Equal(urls[0], result.ImageUrl);
-    //}
-
-
-    //[Fact]
-    //public void BuildDescription_WithEmptyUrls_ReturnsModelWithDefaultImageUrl()
-    //{
-    //    var urls = new List<Uri>();
-    //    var service = new OpenGraphService(_localizer);
-    //    var entry = new Entry(Guid.NewGuid(), Description, DateTime.Now, DateTime.Now, EditMode.Unknown, urls, EntryOpenGraphDescription.Empty);
-
-    //    var result = service.BuildDescription(entry);
-
-    //    Assert.NotNull(result.ImageUrl);
-    //}
+        Assert.Equal(expected, result.Description);
+    }
 
 
     private const string DefaultDescription = "DefaultOpenGraphDescriptionText";
     private const string Description = "This is a test description.";
+    private Uri DefaultUri = new("https://example.com/image.jpg");
 
     private readonly IStringLocalizer<ServerResources> _localizer;
 }
