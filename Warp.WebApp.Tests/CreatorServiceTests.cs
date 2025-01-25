413cecabe15be4e5ac3ffddbbd97a0e9b2b34aa7
Дмитрий Дмитрий <alemont1998@gmail.com>
2025-01-25 20:22:32 +0300

    AttachEntry of CreatorService tests

--- Warp.WebApp.Tests/CreatorServiceTests.cs
+++ Warp.WebApp.Tests/CreatorServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http.HttpResults;
+﻿using CSharpFunctionalExtensions;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.Extensions.Localization;
 using Moq;
 using NSubstitute;
@@ -8,7 +9,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Warp.WebApp.Data;
+using Warp.WebApp.Extensions;
+using Warp.WebApp.Models;
 using Warp.WebApp.Models.Creators;
+using Warp.WebApp.Services;
 using Warp.WebApp.Services.Creators;
 
 namespace Warp.WebApp.Tests;
@@ -50,6 +54,127 @@ public class CreatorServiceTests
         Assert.False(result.IsSuccess);
         Assert.NotNull(result.Error);
         Assert.Equal(400, result.Error.Status);
+    }
+
+    [Fact]
+    public async Task AttachEntry_WithoutExistingEntries_AttachesSuccessfully()
+    {
+        var creator = new Creator(Guid.NewGuid());
+        var entryInfo = new EntryInfo(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            DateTime.Now, 
+            DateTime.Now.AddDays(1), 
+            Models.Entries.Enums.EditMode.Text, 
+            new Entry("Some content"), 
+            new List<ImageInfo>(), 
+            new Models.Entries.EntryOpenGraphDescription("Some content", "Some content", null),
+            0);
+
+        var userIdCacheKey = CacheKeyBuilder.BuildCreatorsEntrySetCacheKey(creator.Id);
+
+        _dataStorageMock
+            .Setup(x => x.TryGetSet<Guid>(userIdCacheKey, It.IsAny<CancellationToken>()))
+            .ReturnsAsync(new HashSet<Guid>());
+
+        _dataStorageMock
+            .Setup(x => x.AddToSet<Guid>(userIdCacheKey, entryInfo.Id, It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+            .ReturnsAsync(Result.Success());
+
+        var result = await _creatorService.AttachEntry(creator, entryInfo, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
+    }
+
+    [Fact]
+    public async Task AttachEntry_AttachFails_ReturnsProblemDetails()
+    {
+        var creator = new Creator(Guid.NewGuid());
+        var entryInfo = new EntryInfo(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            DateTime.Now,
+            DateTime.Now.AddDays(1),
+            Models.Entries.Enums.EditMode.Text,
+            new Entry("Some content"),
+            new List<ImageInfo>(),
+            new Models.Entries.EntryOpenGraphDescription("Some content", "Some content", null),
+            0);
+
+
+        var userIdCacheKey = CacheKeyBuilder.BuildCreatorsEntrySetCacheKey(creator.Id);
+
+        _dataStorageMock.Setup(ds => ds.TryGetSet<Guid>(userIdCacheKey, It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(new HashSet<Guid>());
+
+        _dataStorageMock.Setup(ds => ds.AddToSet(userIdCacheKey, entryInfo.Id, It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(Result.Failure("Failure"));
+
+        var result = await _creatorService.AttachEntry(creator, entryInfo, CancellationToken.None);
+
+        Assert.True(result.IsFailure);
+    }
+
+    [Fact]
+    public async Task AttachEntry_WithExistingEntries_AttachesSuccessfully()
+    {
+        var creator = new Creator(Guid.NewGuid());
+        var entryInfo = new EntryInfo(
+            Guid.NewGuid(),
+            Guid.NewGuid(),
+            DateTime.Now,
+            DateTime.Now.AddDays(1),
+            Models.Entries.Enums.EditMode.Text,
+            new Entry("Some content"),
+            new List<ImageInfo>(),
+            new Models.Entries.EntryOpenGraphDescription("Some content", "Some content", null),
+            0);
+
+        var userIdCacheKey = CacheKeyBuilder.BuildCreatorsEntrySetCacheKey(creator.Id);
+
+        var existingEntryIds = new HashSet<Guid> { Guid.NewGuid(), Guid.NewGuid() };
+
+        var existingEntryInfoFirst = new EntryInfo(
+            existingEntryIds.First(),
+            Guid.NewGuid(),
+            DateTime.Now,
+            DateTime.Now.AddDays(1),
+            Models.Entries.Enums.EditMode.Text,
+            new Entry("Some content"),
+            new List<ImageInfo>(),
+            new Models.Entries.EntryOpenGraphDescription("Some content", "Some content", null),
+            0);
+
+        var existingEntryInfoSecond = new EntryInfo(
+            existingEntryIds.Last(),
+            Guid.NewGuid(),
+            DateTime.Now,
+            DateTime.Now.AddDays(1),
+            Models.Entries.Enums.EditMode.Text,
+            new Entry("Some content"),
+            new List<ImageInfo>(),
+            new Models.Entries.EntryOpenGraphDescription("Some content", "Some content", null),
+            0);
+
+        _dataStorageMock.Setup(ds => ds.TryGetSet<Guid>(userIdCacheKey, It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(existingEntryIds);
+
+        _dataStorageMock.Setup(ds => ds.TryGet<EntryInfo>(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                        .ReturnsAsync((string key, CancellationToken _) =>
+                        {
+                            if (key.Contains(existingEntryInfoFirst.Id.ToString()))
+                                return existingEntryInfoFirst;
+                            if (key.Contains(existingEntryInfoFirst.Id.ToString()))
+                                return existingEntryInfoFirst;
+                            return default;
+                        });
+
+        _dataStorageMock.Setup(ds => ds.AddToSet(userIdCacheKey, entryInfo.Id, It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                        .ReturnsAsync(Result.Success());
+
+        var result = await _creatorService.AttachEntry(creator, entryInfo, CancellationToken.None);
+
+        Assert.True(result.IsSuccess);
     }
 
     private readonly Mock<IDataStorage> _dataStorageMock = new();
