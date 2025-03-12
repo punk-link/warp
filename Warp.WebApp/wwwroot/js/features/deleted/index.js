84a2c0dd3e6d775aae172f6c0e49c2dd95491c90
Kirill Taran <kirill.taran@hotmail.com>
2025-03-12 13:17:46 +0400

    Refactor event handling for deleted items

---
+++ Warp.WebApp/wwwroot/js/features/deleted/index.js
@@ -0,0 +1,23 @@
+﻿import { ROUTES, redirectTo } from '/js/utils/routes.js';
+import { elements } from './elements.js';
+
+
+const handlers = {
+    actions: (() => {
+        const handleCreate = () => {
+            redirectTo(ROUTES.ROOT);
+        };
+
+        return {
+            init: () => {
+                const { create } = elements.getActionButtons();
+                create.addEventListener('click', handleCreate);
+            }
+        };
+    })()
+};
+
+
+export const addDeletedEvents = () => {
+    handlers.actions.init();
+};
