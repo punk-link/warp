48bd8ffaddf7ef28264cd988e182d93c8cdfd892
Kirill Taran <kirill.taran@hotmail.com>
2025-04-30 11:54:43 +0400

    Refactor DataStorage for domain-driven error handling.

--- Warp.WebApp/Data/IDataStorage.cs
+++ Warp.WebApp/Data/IDataStorage.cs
@@ -1,15 +1,16 @@
 ﻿using CSharpFunctionalExtensions;
+using Warp.WebApp.Models.Errors;
 
 namespace Warp.WebApp.Data;
 
 public interface IDataStorage
 {
     public Task<long> AddAndGetCounter(string key, CancellationToken cancellationToken);
-    public Task<Result> AddToSet<T>(string key, T value, TimeSpan expiresIn, CancellationToken cancellationToken);
+    public Task<UnitResult<DomainError>> AddToSet<T>(string key, T value, TimeSpan expiresIn, CancellationToken cancellationToken);
     public ValueTask<bool> Contains<T>(string key, CancellationToken cancellationToken);
     public ValueTask<bool> ContainsInSet<T>(string key, T value, CancellationToken cancellationToken);
     public Task Remove<T>(string key, CancellationToken cancellationToken);
-    public Task<Result> Set<T>(string key, T value, TimeSpan expiresIn, CancellationToken cancellationToken);
+    public Task<UnitResult<DomainError>> Set<T>(string key, T value, TimeSpan expiresIn, CancellationToken cancellationToken);
     public ValueTask<T?> TryGet<T>(string key, CancellationToken cancellationToken);
     public ValueTask<HashSet<T>> TryGetSet<T>(string key, CancellationToken cancellationToken);
 }
