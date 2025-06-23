4cc3b284f066e9e84eb831893ebb96476af76bb4
Kirill Taran <kirill.taran@hotmail.com>
2025-06-23 12:25:36 -0300

    Add validation for EditMode in EntryInfoValidator

--- Warp.WebApp/Models/Validators/EntryInfoValidator.cs
+++ Warp.WebApp/Models/Validators/EntryInfoValidator.cs
@@ -1,5 +1,6 @@
 ﻿using FluentValidation;
 using Warp.WebApp.Extensions;
+using Warp.WebApp.Models.Entries.Enums;
 using Warp.WebApp.Models.Errors;
 
 namespace Warp.WebApp.Models.Validators;
@@ -12,5 +13,10 @@ public class EntryInfoValidator : AbstractValidator<EntryInfo>
         RuleFor(x => x.ExpiresAt).GreaterThan(default(DateTime))
             .WithErrorCode(error.Code.ToHttpStatusCodeInt().ToString())
             .WithMessage(error.Detail);
+
+        var modeError = DomainErrors.EntryEditModeUnset();
+        RuleFor(x => x.EditMode).NotEqual(EditMode.Unset)
+            .WithErrorCode(modeError.Code.ToHttpStatusCodeInt().ToString())
+            .WithMessage(modeError.Detail);
     }
 }
