ec4199fb83a7108cd72ac5f4a8a8edf137ddbb0f
Kirill Taran <kirill.taran@hotmail.com>
2025-08-28 17:57:12 -0300

    Add logo click handler to navigate and clear draft.

--- Warp.ClientApp/src/components/Logo.vue
+++ Warp.ClientApp/src/components/Logo.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="flex">
-    <h1 class="logo-style">
-      <RouterLink to="/" class="text-xl">Warplyn</RouterLink>
-    </h1>
-    <span class="px-2 text-gray-300 text-xs">beta</span>
-  </div>
+    <div class="flex">
+        <h1 class="logo-style">
+            <RouterLink to="/" class="text-xl" @click.prevent="onLogoClick">Warplyn</RouterLink>
+        </h1>
+        <span class="px-2 text-gray-300 text-xs">beta</span>
+    </div>
 </template>
+
+<script setup lang="ts">
+import { useDraftEntry } from '../composables/useDraftEntry'
+import { useRouter } from 'vue-router'
+
+
+const { clearDraft } = useDraftEntry()
+const router = useRouter()
+
+
+function onLogoClick() {
+    clearDraft()
+    router.push({ name: 'Home' })
+}
+</script>
