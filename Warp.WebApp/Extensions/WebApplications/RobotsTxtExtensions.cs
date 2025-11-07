c3a1f4142c237d71f3abee0158136b03d5cc3fee
Kirill Taran <kirill.taran@hotmail.com>
2025-11-07 19:10:43 -0300

    Refactor WebApplicationExtensions for modularity.

---
+++ Warp.WebApp/Extensions/WebApplications/RobotsTxtExtensions.cs
@@ -0,0 +1,23 @@
+﻿namespace Warp.WebApp.Extensions.WebApplications;
+
+internal static class RobotsTxtExtensions
+{
+    internal static WebApplication MapRobotsTxt(this WebApplication app)
+    {
+        app.MapGet("/robots.txt", async (IHostEnvironment env, HttpContext ctx) =>
+        {
+            var filePath = Path.Combine(env.ContentRootPath, "robots.txt");
+            if (!File.Exists(filePath))
+            {
+                ctx.Response.StatusCode = 404;
+                return;
+            }
+
+            var content = await File.ReadAllTextAsync(filePath);
+            ctx.Response.ContentType = "text/plain";
+            await ctx.Response.WriteAsync(content);
+        });
+
+        return app;
+    }
+}
