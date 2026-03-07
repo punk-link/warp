6f1a368c0c34d6ad193ec55a969b1f04fcb8a290
Kirill Taran <kirill.taran@hotmail.com>
2026-03-07 12:37:34 -0300

    Exclude production environment from Consul variables logging

--- Warp.WebApp/Helpers/Configuration/ConfigurationProviders/ConsulConfigurationSource.cs
+++ Warp.WebApp/Helpers/Configuration/ConfigurationProviders/ConsulConfigurationSource.cs
@@ -2,9 +2,10 @@
 
 public class ConsulConfigurationSource : IConfigurationSource
 {
-    public ConsulConfigurationSource(string address, string token, string storageName, ILogger logger)
+    public ConsulConfigurationSource(string address, string token, string storageName, IWebHostEnvironment environment, ILogger logger)
     {
         _address = address;
+        _environment = environment;
         _logger = logger;
         _storageName = storageName;
         _token = token;
@@ -12,10 +13,11 @@ public class ConsulConfigurationSource : IConfigurationSource
 
 
     public IConfigurationProvider Build(IConfigurationBuilder _)
-        => new ConsulConfigurationProvider(_address, _token, _storageName, _logger);
+        => new ConsulConfigurationProvider(_address, _token, _storageName, _environment, _logger);
 
 
     private readonly string _address;
+    private readonly IWebHostEnvironment _environment;
     private readonly ILogger _logger;
     private readonly string _storageName;
     private readonly string _token;
